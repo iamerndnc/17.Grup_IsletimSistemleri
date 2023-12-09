97b1c698b1b0ebb8e54aa39d7c42c70cc966db2a
Eren Dinç <iamerndnc@gmail.com>
2023-12-09 21:17:03 +0300

    Queue.java eklendi

---
+++ src/project/Queue.java
@@ -0,0 +1,36 @@
+package project;
+
+import java.util.LinkedList;
+import java.util.Random;
+
+public class Queue {
+        private LinkedList<Process> queue = new LinkedList<Process>();
+
+    public void PushQueue(Process process) {
+        queue.addLast(process);
+
+    }
+
+    public Process PopQueue() {
+        return queue.remove();
+    }
+
+    public Process PopQueue(int id) {
+        return queue.remove(id);
+    }
+
+    public Process Pull(int index)
+    {
+        return queue.get(index);
+    }
+
+    public boolean isQueueEmpty() {
+        return queue.isEmpty();
+    }
+
+    public int QueueSize()
+    {
+        return queue.size();
+    }
+
+}
