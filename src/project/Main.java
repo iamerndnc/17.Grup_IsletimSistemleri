a13998ba6d2dffa792a97008874b63bf042ff926
Nihad Asgarov <askhervn@gmail.com>
2023-12-27 16:35:45 +0300

    Odev duzenleme

---
+++ src/project/Main.java
@@ -0,0 +1,16 @@
+package project;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Program pr = new Program();
+
+        File p = new File(pr);
+
+        p.reader();
+
+    }
+
+}
---
+++ src/project/UserQueue.java
@@ -0,0 +1,31 @@
+package project;
+
+import java.io.File;
+
+public class UserQueue {
+	Queue qu = new Queue();
+
+	void UserAdd(Process process) {
+		qu.PushQueue(process);
+	}
+	
+	void UserProgram()
+	{
+		Program prog = File.prog;
+		
+		while(!qu.isQueueEmpty())
+		{
+			if (qu.Pull(0).priority == 2) 
+			{
+				prog.spl.queue2.PushQueue(qu.PopQueue());
+			} else if (qu.Pull(0).priority == 1) 
+			{
+				prog.fpl.queue1.PushQueue(qu.PopQueue());
+			} else 
+			{
+				prog.rr.qu.PushQueue(qu.PopQueue());
+			}
+		}
+		
+	}
+} 
